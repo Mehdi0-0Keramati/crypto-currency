daa7e063044c49e5dbc92b055732e7930e903a4a
MehdiKeramati <0mehdikeramati1@gmail.com>
2022-09-09 18:07:02 +0430

    add coins

--- src/coins.jsx
+++ src/coins.jsx
@@ -8,7 +8,7 @@ const Coins = () => {
     const [Loading, setIsLoading] = useState(true)
 
     useEffect(() => {
-        const url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=4&page=1&sparkline=false"
+        const url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
 
         axios.get(url, setIsLoading(false)).then((res) => {
             setData(res.data)
